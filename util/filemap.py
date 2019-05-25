6df8f7ab4894c1786a18d22c337ff57ef968e941
Cole Robinson <crobinso@redhat.com>
2019-05-25 15:45:03 -0400

    filemap: Fix some long line warnings

--- util/filemap.py
+++ util/filemap.py
@@ -320,11 +320,13 @@ DRIVER_OS_MAP = {
         'Win7/amd64': ['w7/amd64', '2k8R2/amd64', '../amd64/w7'],
 
         'Win8/x86': ['w8/x86', 'w8.1/x86', '../i386/w8'],
-        'Win8/amd64': ['w8/amd64', 'w8.1/amd64', '2k12/amd64', '2k12R2/amd64', '../amd64/w8'],
+        'Win8/amd64': ['w8/amd64', 'w8.1/amd64', '2k12/amd64', '2k12R2/amd64',
+                       '../amd64/w8'],
 
         'Win10/x86': ['w10/x86', '../i386/w10'],
-        'Win10/amd64': ['w10/amd64', '2k16/amd64', '2k19/amd64', '../amd64/w10'],
-        'Win10/ARM64': ['w10/ARM64','../ARM64/w10'],
+        'Win10/amd64': ['w10/amd64', '2k16/amd64', '2k19/amd64',
+                        '../amd64/w10'],
+        'Win10/ARM64': ['w10/ARM64', '../ARM64/w10'],
     },
 
 
@@ -362,10 +364,12 @@ DRIVER_OS_MAP = {
         'Win7/amd64': ['w7/amd64', '2k8R2/amd64', '../amd64/w7'],
 
         'Win8/x86': ['w8/x86', 'w8.1/x86', '../i386/w8'],
-        'Win8/amd64': ['w8/amd64', 'w8.1/amd64', '2k12/amd64', '2k12R2/amd64', '../i386/w8'],
+        'Win8/amd64': ['w8/amd64', 'w8.1/amd64', '2k12/amd64', '2k12R2/amd64',
+                       '../i386/w8'],
 
         'Win10/x86': ['w10/x86', '../i386/w10'],
-        'Win10/amd64': ['w10/amd64', '2k16/amd64', '2k19/amd64', '../amd64/w10'],
+        'Win10/amd64': ['w10/amd64', '2k16/amd64', '2k19/amd64',
+                        '../amd64/w10'],
         'Win10/ARM64': ['w10/ARM64', '../ARM64/w10'],
     },
 
